72f462eb4600774ca4d20cfbe7c7d21be416ad07
CamilaCarrenoV <camila.carreno89@gmail.com>
2024-05-31 17:48:07 -0400

    conteo de números

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,28 +1,42 @@
 const analyzer = {  
   getWordCount: (text) => {
-    
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  return 
+    let palabras = text.trim().split(/\s+/);
+    return palabras.length;
   },
+
+ //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-  return text.length;
+    return text.length;
   },
+  //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+  
   getCharacterCountExcludingSpaces: (text) => {
     let sinespacionipuntuacion = text.replace(/[^a-zA-Z0-9]/g, '');
     return sinespacionipuntuacion.length;
 
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+
+  getAverageWordLength: (text) => {  
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getNumberCount: (text) => {
+    const regex = /\d+/g;
+    const coincidencias = text.match(regex);
+    if(!coincidencias){
+      return 0;
+    }
+    return coincidencias.length;
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getNumberSum: (text) => {
+    
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
+
 };
 
 export default analyzer;
