7d20a4a4ea9293767f5e61cfff31a58e9b2b5ba9
CamilaCarrenoV <camila.carreno89@gmail.com>
2024-06-06 18:25:26 -0400

    Solo un error

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,4 +1,5 @@
 const analyzer = {
+
   getWordCount: (text) => {
     const palabras = text.trim().split(/\s+/);
     if (palabras[0].length === 0) {
@@ -15,24 +16,27 @@ const analyzer = {
   //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
 
   getCharacterCountExcludingSpaces: (text) => {
-    const puntuacion = /[/[;:!?¿¡()"'`´“”‘’\]{}<>\-—_…«»\d]/g;
-    const sinespacionipuntuacion = text.replace(puntuacion,'');
+    const sinespacionipuntuacion = text.replace(/[^a-zA-Z0-9]/g, '');
     return sinespacionipuntuacion.length;
 
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
 
   getAverageWordLength: (text) => {
-    const puntua = /[/[;:!?¿¡()"'`´“”‘’\]{}<>\-—_…«»\d]/g;
-    const newtext = text.replace(puntua,'');
-    const words = newtext.split('');
-    const resultadopalabras = words.length;
-    const longitudtotal = words.reduce((total, palabra) => total + palabra.length, 0)
-    const longitudmedia = longitudtotal / resultadopalabras
-    return parseFloat(longitudmedia.toFixed(2))
+    const nuevotexto = text.replace(/[^a-zA-Z0-9]/g,"")
+    const caractotal = nuevotexto.length;
+    const word = text.split(/\s+/);
+    const totalpalabr = word.length;
+    
+    if (totalpalabr === 0) {
+      return 0;
+    }
+    const promedio = caractotal / totalpalabr;
+    return promedio.toFixed(2);
+
+    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
-  //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  
+
   getNumberCount: (text) => {
     const regex = /\b\d+(\.\d+)?\b/g;
     const coincidencias = text.match(regex);
